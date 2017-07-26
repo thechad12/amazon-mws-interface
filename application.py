588075f59bb9edab34abfecdb122b2304248b0db
chalktalksports <kpasquarella@chalktalksports.com>
2017-07-26 12:33:42 -0400

    add check to see if feed variable is string, properly parse, try except block for response

--- application.py
+++ application.py
@@ -1,27 +1,25 @@
-from flask import Flask, render_template, request, make_response, send_from_directory, redirect, url_for, Response, flash
+from flask import Flask, render_template, request, make_response, send_from_directory, redirect, url_for, Response, flash, session
 from werkzeug.utils import secure_filename
+from werkzeug.local import Local, LocalManager
 from requests_toolbelt import MultipartEncoder
 import requests
 import boto
 from boto.mws import connection
+import boto.mws.response
+import boto.mws.exception
 from boto.mws.connection import MWSConnection
 import itertools
 import os
-import gc
-import json
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database import Base, FeedResult
+import re
+import json
+import httplib2
 from flask_wtf.csrf import CsrfProtect
+import xml.etree.ElementTree as et
 
 
 csrf = CsrfProtect()
-
-engine = create_engine('sqlite:///amazon.db')
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
 app = Flask(__name__)
 
@@ -44,9 +42,6 @@ MARKETPLACE_ID = json.loads(open(
     'amazon.json','r').read())['web']['marketplace_id']
 
 ACCOUNT_TYPE = "Merchant"
-
-conn = connection.MWSConnection(aws_access_key_id=MWS_ACCESS_KEY,
-        aws_secret_access_key=MWS_SECRET_KEY, Merchant=MERCHANT_ID)
 
 @app.route('/')
 @app.route('/home/')
@@ -78,29 +73,53 @@ def feed_operation(f_name):
 def feed_submission():
     if request.method == 'POST':
             file = request.files['file']
+
             # Only XML files allowed
             if not allowed_filetype(file.filename):
                 output = '<h2 style="color:red">Filetype must be XML! Please upload an XML file.</h2>'
                 return output
                 raise ValueError('Filetype Must Be XML.')
+
             # Read file and encode it for transfer to Amazon
             file_name = request.form['file_name']
             f = file.read()
             u = f.decode("utf-8-sig")
             c = u.encode("utf-8")
             feed_content = c
-            submit_feed_response = conn.submit_feed(
+
+            conn = connection.MWSConnection(aws_access_key_id=MWS_ACCESS_KEY,
+            aws_secret_access_key=MWS_SECRET_KEY, Merchant=MERCHANT_ID)
+
+            feed = conn.submit_feed(
                 FeedType=feed_operation(file_name),
                 PurgeAndReplace=False,
                 MarketplaceIdList=[MARKETPLACE_ID],
                 content_type='text/xml',
                 FeedContent=feed_content)
-            feed_info = submit_feed_response.SubmitFeedResult.FeedSubmissionInfo.FeedSubmissionId
+
+            if type(feed) is not str:
+                feed_info = feed.SubmitFeedResult.FeedSubmissionInfo.FeedSubmissionId
+            else:
+                tree = et.fromstring(feed)
+                xmlns = {'response': '{http://mws.amazonaws.com/doc/2009-01-01/}'}
+                info = tree.find('.//{response}FeedSubmissionId'.format(**xmlns))
+                feed_info = info.text
+
+            flash('Submitted Product Feed: ' + str(feed_info))
             return redirect(url_for('feed_result', feed_id=feed_info))
+
     else:
         return render_template('submit_feed.html',access_key=MWS_ACCESS_KEY,secret_key=MWS_SECRET_KEY,
             merchant_id=MERCHANT_ID,marketplace_id=MARKETPLACE_ID)
 
+@app.route('/feed-history')
+def feed_history():
+    response = MWSConnection._parse_response = lambda s,x,y,z: z
+    conn = connection.MWSConnection(aws_access_key_id=MWS_ACCESS_KEY,
+            aws_secret_access_key=MWS_SECRET_KEY, Merchant=MERCHANT_ID)
+    feed_history = conn.get_feed_submission_list()
+    response = Response(feed_history, mimetype='text/xml')
+    return response
 
 @csrf.error_handler
 def csrf_error(error):
@@ -109,10 +128,18 @@ def csrf_error(error):
 # Retrieve submitted feed result
 @app.route('/feed-result/<int:feed_id>')
 def feed_result(feed_id):
-    response = MWSConnection._parse_response = lambda s,x,y,z: z
-    submitted_feed = conn.get_feed_submission_result(FeedSubmissionId=feed_id)
-    result = Response(submitted_feed, mimetype='text/xml')
-    return(result)
+    try:
+        response = MWSConnection._parse_response = lambda s,x,y,z: z
+        conn = connection.MWSConnection(aws_access_key_id=MWS_ACCESS_KEY,
+            aws_secret_access_key=MWS_SECRET_KEY, Merchant=MERCHANT_ID)
+
+        submitted_feed = conn.get_feed_submission_result(FeedSubmissionId=feed_id)
+
+        response = Response(submitted_feed, mimetype='text/xml')
+        return response
+    except:
+        output = '<h2>Feed processing result not ready. Please refresh in a minute</h2>'
+        return output
 
 if __name__ == '__main__':
     app.debug = True
